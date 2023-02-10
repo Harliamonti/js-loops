a2b2b7bf915654a1069d5a003cc6909d46c264da
Harliamonti <123170910+Harliamonti@users.noreply.github.com>
2023-02-10 10:47:28 +0200

    Add basic loops

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
-console.log("Hello World!");
+const testArray = ["testy", "Liam", "javaSCRIPT"];
+
+for (const it of testArray)
+{
+    console.log(it);
+}
+
+console.log("\n")
+
+for (let i = 0; i < testArray.length; i++)
+{
+    console.log(testArray[i]);
+}
+
+const foo = testArray.map(string => string.toUpperCase());
+
+for (const it of foo)
+{
+    console.log(it);
+}
